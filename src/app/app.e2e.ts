12a58c3b1fa8aef888f0e3e44ddba1789a758f2f
Pavlo Glazkov <p.glazkov@gmail.com>
2017-01-15 13:23:21 +0100

    Added a test.

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -1,4 +1,4 @@
-import { browser } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 describe('App', () => {
 
@@ -6,10 +6,15 @@ describe('App', () => {
     browser.get('/');
   });
 
-
   it('should have a title', () => {
     let subject = browser.getTitle();
     let result = 'IndependerStarter';
+    expect(subject).toEqual(result);
+  });
+
+  it('should have <home>', () => {
+    let subject = element(by.css('app home')).isPresent();
+    let result = true;
     expect(subject).toEqual(result);
   });
 
