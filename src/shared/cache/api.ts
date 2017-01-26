d62aefaa65ab40773080931c69662c1b0b25c3b4
Pavlo Glazkov <p.glazkov@gmail.com>
2017-01-26 07:14:03 +0100

    Updated packages and re-saved files with proper encoding (tslint now complains).

--- src/shared/cache/api.ts
+++ src/shared/cache/api.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, RequestOptionsArgs } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
