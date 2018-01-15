696eba27a8f21c5b8507db454712b082ea017a42
Pavlo Glazkov <p.glazkov@gmail.com>
2018-01-15 16:14:22 +0100

    Remove accidentally committed `.js` file

--- src/apps/first/main.browser.aot.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var app_bootstrap_aot_1 = require("../../shared/misc/app-bootstrap-aot");
-var app_browser_module_ngfactory_1 = require("../../../aot_temp/src/apps/first/app/app.browser.module.ngfactory");
-app_bootstrap_aot_1.bootstrapAot(app_browser_module_ngfactory_1.AppBrowserModuleNgFactory);
